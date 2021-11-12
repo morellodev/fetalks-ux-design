cf6516977502c3b378dec7e73884b345889d6f5f
Dennis Morello <dennis.morello@contentwise.com>
2021-11-12 15:25:21 +0100

    perf(ant): improved antd build size

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import styleImport from "vite-plugin-style-import";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
+  plugins: [
+    react(),
+    styleImport({
+      libs: [
+        {
+          libraryName: "antd",
+          esModule: true,
+          resolveStyle: (name) => `antd/es/${name}/style/index`,
+        },
+      ],
+    }),
+  ],
 });
